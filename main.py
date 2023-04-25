fd0e92c158bf05d07bf8f53049421704ee56adf4
ijlm <ivan.laverde.85@gmail.com>
2023-04-24 21:30:01 -0500

    primera entrada

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class parte1(BaseModel):
+    parte: str
+    parte2: str
+    parte3: str
+
+app=FastAPI()
+
+@app.get("/")
+
+def index():
+    return "hola"
+
+@app.get("/ruta/{id}")
+def motrar_hoja(id):
+    return {"ruta":id}
+
+@app.post("/parte1")
+def insertar(parte: parte1):
+    return{"esta en la parte indicada": parte.parte2}
